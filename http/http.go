59ca648d1f587dd4b55772a9f29696ba4af9337e
Lambels <patrick.arvatu@yahoo.com>
2023-01-08 13:42:30 +0200

    moved error http handling

--- http/http.go
+++ http/http.go
@@ -32,7 +32,7 @@ func SendErr(w http.ResponseWriter, r *http.Request, err error) {
 		LogError(r, err)
 	}
 
-	status := FromErrorCodeToStatus(code)
+	status := csb.FromErrorCodeToStatus(code)
 	w.WriteHeader(status)
 	WriteJSON(w, errResponse{Status: status, Trace: message})
 }
@@ -44,33 +44,4 @@ func LogError(r *http.Request, err error) {
 func WriteJSON(w io.Writer, data interface{}) error {
 	enc := json.NewEncoder(w)
 	return enc.Encode(data)
-}
-
-var codes = map[string]int{
-	csb.ECONFLICT:       http.StatusConflict,
-	csb.EINVALID:        http.StatusBadRequest,
-	csb.ENOTFOUND:       http.StatusNotFound,
-	csb.ENOTIMPLEMENTED: http.StatusNotImplemented,
-	csb.EUNAUTHORIZED:   http.StatusUnauthorized,
-	csb.EINTERNAL:       http.StatusInternalServerError,
-}
-
-// FromErrorCodeToStatus maps a csb error code to a http status code, if no mapping is possible
-// status code 500 is returned.
-func FromErrorCodeToStatus(code string) int {
-	if v, ok := codes[code]; ok {
-		return v
-	}
-	return http.StatusInternalServerError
-}
-
-// FromStatusToErrorCode maps a http status code to a csb error code, if no mapping is possible
-// csb.EINTERNAL is returned.
-func FromStatusToErrorCode(code int) string {
-	for k, v := range codes {
-		if v == code {
-			return k
-		}
-	}
-	return csb.EINTERNAL
 }
